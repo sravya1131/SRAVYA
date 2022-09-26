bcd490b57d8a6ea1cb90d1410511394bc30d1241
sravya <112858180+sravya1131@users.noreply.github.com>
2022-09-26 10:52:46 +0530

    Create graph.h

---
+++ graph.h
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+CSE={"c1":55 , "c2":62 , "c3":98}
+batch=list(CSE.keys())
+passpercentage=list(CSE.values())
+fig=plt.figure(figsize=(10,6))
+plt.barh(y=batch , width=passpercentage,
+        colors=["blue" ,"pink" , "red"],
+     
+        edgrcolor="black",
+        tick_label=["c1","c2","c3"],
+
+        )
+        plt.ylabel(Batch)
+        plt.xlabel(passpercentage)
+        plt.title("CSE")
